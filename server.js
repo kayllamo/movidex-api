d80ad9520c53c467831b1a0bfe2c6cbd2e217791
kaylajaograham <kaylajaograham@gmail.com>
2019-09-07 11:16:26 -0700

    deleting tests

--- server.js
+++ server.js
@@ -65,6 +65,8 @@ app.use((error, req, res, next) => {
   res.status(500).json(response)
 })
 
-app.listen(process.env.PORT || 8000, () => {
+const PORT = process.env.PORT || 8000
+
+app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`)
 })
