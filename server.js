2ca0e8a34daf92210c4960f82d7fa88679574866
kaylajaograham <kaylajaograham@gmail.com>
2019-09-07 10:16:07 -0700

    heroku changes

--- server.js
+++ server.js
@@ -65,8 +65,8 @@ app.use((error, req, res, next) => {
   res.status(500).json(response)
 })
 
-const PORT = process.env.PORT || 8000
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
-  // console.log(`Server listening at http://localhost:${PORT}`)
+  console.log(`Server listening at http://localhost:${PORT}`)
 })
